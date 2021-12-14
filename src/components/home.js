d751541b28960ea8efe4758de3f3cbece960607c
Daniel Arpide <djarpide@gmail.com>
2021-12-14 16:21:20 -0500

    Fixed homepage

--- src/components/home.js
+++ src/components/home.js
@@ -1,20 +1,23 @@
 import Hero from './hero'
 
 const Home = () => {
-    return (
-        <>
-            <Hero text="Welcome to React 201" />
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-8 offset-lg-2 my-5">
-                        <p className="lead">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum sint maiores aliquid, veniam pariatur vel placeat dolore laudantium, adipisci eum odit dolores rerum, id dolor laborum doloribus autem! Modi, vero!
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Hero text='Welcome to the Movie Browser' />
+      <div className='container'>
+        <div className='row'>
+          <div className='col-lg-8 offset-lg-2 my-5'>
+            <p className='lead'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+              sint maiores aliquid, veniam pariatur vel placeat dolore
+              laudantium, adipisci eum odit dolores rerum, id dolor laborum
+              doloribus autem! Modi, vero!
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default Home;
+export default Home
